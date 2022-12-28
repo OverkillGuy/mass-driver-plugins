716d40229cf4f209e99e3f6b35c6f343d7e96c73
Jb DOYON <jb@jiby.tech>
2022-12-27 21:47:39 -0400

    Move in complex PatchDrivers as plugins
    Moved away from mass-driver repo itself due to need for dependencies.

--- src/mass_driver_plugins/cli.py
+++ src/mass_driver_plugins/cli.py
@@ -3,6 +3,8 @@ import argparse
 import sys
 from typing import Optional
 
+from mass_driver.discovery import discover_drivers
+
 
 def parse_arguments(arguments: list[str]) -> argparse.Namespace:
     """Parse generic arguments, given as parameters"""
@@ -10,7 +12,6 @@ def parse_arguments(arguments: list[str]) -> argparse.Namespace:
         "mass-driver-plugins",
         description="Experimental plugin ecosystem for Mass Driver",
     )
-    parser.add_argument("foo", help="Some parameter")
     return parser.parse_args(arguments)
 
 
@@ -18,10 +19,10 @@ def cli(arguments: Optional[list[str]] = None):
     """Run the mass_driver_plugins cli"""
     if arguments is None:
         arguments = sys.argv[1:]
-    args = parse_arguments(arguments)
-    main(args.foo)
+    parse_arguments(arguments)
+    main()
 
 
-def main(foo):
+def main():
     """Run the program's main command"""
-    print(f"Foo is: {foo}")
+    print("\n".join(discover_drivers()))
