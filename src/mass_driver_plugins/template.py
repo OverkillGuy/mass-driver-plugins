12229a7240c336dc3c9df10c3efb2f9a17c9cada
Jb DOYON <jb@jiby.tech>
2023-02-05 00:41:32 +0000

    Add templater PatchDriver w/ tests

---
+++ src/mass_driver_plugins/template.py
@@ -0,0 +1,30 @@
+"""Create templated files via Jinja2"""
+from pathlib import Path
+
+from typing import Any
+from jinja2 import Environment
+from mass_driver.patchdriver import PatchDriver, PatchOutcome, PatchResult
+
+
+class TemplatedFile(PatchDriver):
+    """Create a file via Jinja template filling"""
+
+    target_file: str
+    """The file to expand via template"""
+    template: str
+    """A jinja2 template"""
+    context: dict[str, Any]
+    """Per repo context for template expansion, with repo name as key"""
+    jinja_args: dict
+    """Extra args to give jinja templating"""
+
+    def run(self, repo: Path) -> PatchResult:
+        """Process the template file"""
+        env = Environment(**self.jinja_args, autoescape=True)
+        template = env.from_string(self.template)
+        repo_name = repo.name
+        context = self.context.get(repo_name)
+        rendered = template.render(context)
+        with open(repo / Path(self.target_file), "w") as fd:
+            fd.write(rendered)
+        return PatchResult(outcome=PatchOutcome.PATCHED_OK)
---
+++ tests/test_template.py
@@ -0,0 +1,31 @@
+"""Validate the template"""
+
+from pathlib import Path
+
+# from mass_driver.migration import Migration
+from mass_driver.patchdriver import PatchOutcome
+from mass_driver.tests.fixtures import copy_folder, massdrive
+
+CONFIG_FILENAME = "template_migration.toml"
+
+
+def test_template_expansion(tmp_path, datadir):
+    """Scenario: Use the template expander PatchDriver"""
+    # Given a sample repo to mass-drive
+    # And sample repo has counter at value 1
+    repo_path = Path(tmp_path / "test_repo/")
+    copy_folder(Path(datadir / "sample_repo"), repo_path)
+    config_filepath = datadir / CONFIG_FILENAME
+    # migration = Migration.from_config(config_filepath.read_text())
+    # When I run mass-driver
+    result = massdrive(
+        repo_path,
+        config_filepath,
+    )
+    assert result.outcome == PatchOutcome.PATCHED_OK, f"Wrong outcome from patching: {result.details}"
+    # target_text_post = (repo_path / migration.driver.target_file).read_text()
+    # # Then the counter is bumped to config value
+    # # Note: Different configfilename set the target_count to different value
+    # assert (
+    #     int(counter_text_post) == migration.driver.target_count
+    # ), "Counter not updated properly"
